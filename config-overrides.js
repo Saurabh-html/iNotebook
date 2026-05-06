c648eaaf95fee3ab0aa703d484bd900606721809
Saurabh Suman <saurabhsuman843325@gmail.com>
2026-05-06 16:34:30 +0530

    Fix webpack polyfill error with react-app-rewired

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,19 @@
+const webpack = require('webpack');
+
 module.exports = function override(config) {
   config.resolve.fallback = {
     ...config.resolve.fallback,
     fs: false,
     path: false,
     os: false,
+    process: require.resolve('process/browser'),
   };
+
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    })
+  );
 
   return config;
 };
